9de78ebe37f530ba607a55e28026b44980ead3b1
suschi1993 <104782950+suschi1993@users.noreply.github.com>
2023-05-02 23:17:02 +0200

    Add config.py file

---
+++ config.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+PATH_TO_FOMC_CSV_INPUT = Path("/home/janspoerer/code/janspoerer/fomc/Data & Results.xlsx - Data & Labels.csv")
+PATH_TO_FOMC_CSV_OUTPUT_FOR_HUGGINGFACE = Path("/home/janspoerer/code/janspoerer/fomc/train.csv")
